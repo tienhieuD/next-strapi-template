d0593c3991a7e3e0620ff231e3515368fe7e8bfd
tuyendv <tuyendv@gmmaill.coooomm>
2022-03-01 10:46:53 +0700

    .

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -5,24 +5,24 @@ import { fetchAPI } from "../lib/api"
 const Home = ({ homepage }) => {
   return (
     <>
-      <Seo seo={homepage.seo} />
-      <div className="uk-section">
+      {/* <Seo seo={homepage.seo} /> */}
+      <div>
         Hello, world!
       </div>
     </>
   )
 }
 
-export async function getStaticProps() {
-  // Run API calls in parallel
-  const [homepage] = await Promise.all([
-    fetchAPI("/homepage"),
-  ])
+// export async function getStaticProps() {
+//   // Run API calls in parallel
+//   const [homepage] = await Promise.all([
+//     fetchAPI("/homepage"),
+//   ])
 
-  return {
-    props: { homepage },
-    revalidate: 1,
-  }
-}
+//   return {
+//     props: { homepage },
+//     revalidate: 1,
+//   }
+// }
 
 export default Home
